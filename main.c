2c10b698ac8b27ec6fe04685731aa912b6b4febe
chahirsaid <chahirsaid18@gmail.com>
2025-03-21 05:07:28 +0000

    so Hell

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ void exit_error()
 	ft_putstr_fd("Error: Openning File\n",2);
 	exit(1);
 }
-int map_len(char *map)
+int map_len(char *map_path)
 {
     int len = 0;
-    int fd = open(map, O_RDONLY);
+    int fd = open(map_path, O_RDONLY);
 	
 	if (fd == -1)
 		exit_error();
@@ -25,9 +25,9 @@ int map_len(char *map)
     return (len);
 }
 
-void check_map(char *map)
+void check_map(char *map_path)
 {
-    int fd = open(map, O_RDONLY);
+    int fd = open(map_path, O_RDONLY);
 	if (fd == -1)
 		exit_error();
     char *line = get_next_line(fd);
@@ -47,7 +47,7 @@ void check_map(char *map)
         {
             free(line);
             close(fd);
-			ft_printf("error");
+			ft_putstr_fd("Error: Map must be rectangular\n", 2);
             exit(1);
         }
         free(line);
@@ -55,9 +55,9 @@ void check_map(char *map)
     }
     close(fd);
 }
-void print_map(char *map)
+void print_map(char *map_path)
 {
-    int fd = open(map, O_RDONLY);
+    int fd = open(map_path, O_RDONLY);
 	if (fd == -1)
 		exit_error();
     char *line = get_next_line(fd);
@@ -70,28 +70,123 @@ void print_map(char *map)
     }
     close(fd);
 }
-int valid_name(char *av)
+int valid_name(char *map_path)
 {
     int len = 0;
     int i;
 
-    if (!av || ft_strncmp(av, "maps/", 5) != 0)
+    if (!map_path || ft_strncmp(map_path, "maps/", 5) != 0)
         return 0;
+    len = ft_strlen(map_path);
+    if (len <= 9)
+        return 0;
+     if (ft_strcmp(&map_path[len - 4], ".ber") != 0)
+         return 0;
     i = 5;
-    if(!av[i])
-        return 0;
-    while (av[i])
+    while (map_path[i])
     {
-        if (av[i] == '/')
+        if (map_path[i] == '/')
             return 0;
-        if (av[i] == '.')
-            len = i;
         i++;
     }
-    if (len <= 5 || ft_strcmp(&av[len], ".ber") != 0)
-        return 0;
     return 1;
 }
+
+void check_borders(char *map_path)
+{
+    char *line;
+    int cline = 0;
+    int line_len = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char **map = NULL;
+    int fd = open("map_path", O_RDONLY);
+    if (fd == -1)
+        exit_error();
+    cline = map_len(map_path);
+    map = malloc(sizeof(char *) * (cline + 1));
+    if (!map)
+        exit(1);
+    line = get_next_line(fd);
+    while (line)
+    {
+        map[i++] = line;
+        if(i == 1)
+            line_len = ft_strlen(line);
+    }
+    map[i] = NULL;
+    close(fd);
+    while (j < line_len - 1)
+    {
+        if (map[0][j] != '1' || map[cline -1][j] != '1')
+        {
+            ft_putstr_fd("Error: Map Must Be Surrounded By Walls\n", 2);
+            while (k++ < cline)
+                free(map[k]);
+            free(map);
+            exit(1);
+        }
+        j++;
+    }
+    j = 0;
+    while(j++ < cline)
+        free(map[j]);
+    free(map);
+}
+
+void check_elements(char *map_path)
+{
+    int fd = open(map_path, O_RDONLY);
+    if (fd == -1)
+        exit_error();
+    
+    char *line;
+    int player_count = 0;
+    int collect_count = 0;
+    int exit_count = 0;
+    int i;
+    
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        i = 0;
+        while (line[i])
+        {
+            if (line[i] == 'P')
+                player_count++;
+            else if (line[i] == 'C')
+                collect_count++;
+            else if (line[i] == 'E')
+                exit_count++;
+            else if (line[i] != '0' && line[i] != '1' && line[i] != '\n')
+            {
+                free(line);
+                close(fd);
+                ft_putstr_fd("Error: Invalid character in map\n", 2);
+                exit(1);
+            }
+            i++;
+        }
+        free(line);
+    }
+    close(fd);
+    if (player_count != 1)
+    {
+        ft_putstr_fd("Error: Map must have exactly one player (P)\n", 2);
+        exit(1);
+    }
+    if (collect_count < 1)
+    {
+        ft_putstr_fd("Error: Map must have at least one collectible (C)\n", 2);
+        exit(1);
+    }
+    if (exit_count != 1)
+    {
+        ft_putstr_fd("Error: Map must have exactly one exit (E)\n", 2);
+        exit(1);
+    }
+}
+
 int main(int ac, char **av)
 {
     if (ac != 2)
@@ -102,5 +197,8 @@ int main(int ac, char **av)
 		exit(1);
 	}
     check_map(av[1]);
+    check_borders(av[1]);
+    check_elements(av[1]);
+    //flood_fill(av[1]);
 	print_map(av[1]);
 }
